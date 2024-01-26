91852d5f02115aa7d4b610eb8eadab229acffe10
Donald Mashiri <mashirid@staff.msu.ac.zw>
2024-01-26 17:07:14 +0200

    first commit

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginRegisterController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+});
+
+// Public routes of product
+Route::controller(ProductController::class)->group(function() {
+    Route::get('/products', 'index');
+    Route::get('/products/{id}', 'show');
+    Route::get('/products/search/{name}', 'search');
+});
+
+Route::middleware('auth:api')->group( function () {
+    Route::post('/logout', [LoginRegisterController::class, 'logout']);
+
+    Route::controller(ProductController::class)->group(function() {
+        Route::post('/products', 'store');
+        Route::post('/products/{id}', 'update');
+        Route::delete('/products/{id}', 'destroy');
+    });
+});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
